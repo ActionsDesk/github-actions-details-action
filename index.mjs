8fca462c9d05850d1c36dce4e46a8b4dcb019b28
Stefan Stölzle <stoe@github.com>
2021-05-27 12:36:54 +0200

    📦 Implement GitHub Action logic

---
+++ index.mjs
@@ -0,0 +1,30 @@
+import {join, parse} from 'path'
+import {getInput, setFailed} from '@actions/core'
+import {context} from '@actions/github'
+
+import ActionDetails from './utils/ActionDetails.mjs'
+
+// action
+;(async () => {
+  try {
+    const searchToken = getInput('search_token', {required: true})
+    const token = getInput('token', {required: true})
+    const allowList = getInput('allow_list_path')
+    const workspace = process.env.GITHUB_WORKSPACE
+
+    const allowListPath = join(workspace, allowList)
+    const {dir} = parse(allowListPath)
+
+    if (dir.indexOf(workspace) < 0) {
+      throw new Error(`${allowList} is not an allowed path`)
+    }
+
+    const ad = new ActionDetails({token, searchToken, allowList, context})
+
+    await ad.getDetails()
+    // await ad.postComment()
+  } catch (error) {
+    console.error(error)
+    setFailed(error.message)
+  }
+})()
